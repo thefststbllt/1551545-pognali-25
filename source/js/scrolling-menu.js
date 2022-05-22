5f7c393d63e2dc37e299ea22bbb11bd2a73a8092
thefststbllt <bel12pasha@yandex.ru>
2022-05-22 13:19:18 +0300

    fix: final fixies

--- source/js/scrolling-menu.js
+++ source/js/scrolling-menu.js
@@ -11,13 +11,12 @@ window.onscroll = function () {
     nav.classList.add('main-nav--sticky');
     nav.classList.add('main-nav--scrolled');
     mainNavList.classList.add('main-nav__list--active');
-  } else if (window.pageYOffset > 0) {
+    burgerButton.classList.add('burger-button--scrolled');
+    mainNav.classList.add('main-nav--active');
     if (ourLogo.classList.contains('main-header__logo--active')) {
       ourLogo.classList.add('main-header__logo--active-scrolled');
     }
-    burgerButton.classList.add('burger-button--scrolled');
-    mainNav.classList.add('main-nav--active');
-  } else if (window.pageYOffset === 0) {
+  } else if (window.pageYOffset < sticky) {
     ourLogo.classList.remove('main-header__logo--active-scrolled');
     nav.classList.remove('main-nav--sticky');
     nav.classList.remove('main-nav--scrolled');
@@ -26,3 +25,24 @@ window.onscroll = function () {
     mainNav.classList.remove('main-nav--active');
   }
 };
+
+// window.onscroll = function () {
+//   if (window.pageYOffset > sticky) {
+//     nav.classList.add('main-nav--sticky');
+//     nav.classList.add('main-nav--scrolled');
+//     mainNavList.classList.add('main-nav__list--active');
+//   } else if (window.pageYOffset > 0) {
+//     if (ourLogo.classList.contains('main-header__logo--active')) {
+//       ourLogo.classList.add('main-header__logo--active-scrolled');
+//     }
+//     burgerButton.classList.add('burger-button--scrolled');
+//     mainNav.classList.add('main-nav--active');
+//   } else if (window.pageYOffset === 0) {
+//     ourLogo.classList.remove('main-header__logo--active-scrolled');
+//     nav.classList.remove('main-nav--sticky');
+//     nav.classList.remove('main-nav--scrolled');
+//     mainNavList.classList.remove('main-nav__list--active');
+//     burgerButton.classList.remove('burger-button--scrolled');
+//     mainNav.classList.remove('main-nav--active');
+//   }
+// };
